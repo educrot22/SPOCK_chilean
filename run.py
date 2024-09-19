67f84d67c54aec85d745fbc252ccb45c7ae34a3a
educrot22 <ducrotelsa@gmail.com>
2024-09-19 13:58:11 +0200

    "change run.py"

--- run.py
+++ run.py
@@ -3,38 +3,36 @@ from SPOCK_chilean.make_night_plans import chilean_time
 import SPOCK_chilean.ETC as ETC
 
 # set in advance with the planning with SSO team
-telescope = 'Callisto'
+telescope = 'Ganymede' # name of telescope allocated
+date = '2021-07-07'  # start of night date
 
-date = '2022-03-08 15:00:00'  # start of night date
-chilean_nb_target = 4  # number of target to observe this night
+
+chilean_nb_target = 2  # number of target to observe this night
 counts = 5000  # counts of images per targets (no limit = 5000)
-chilean_plans = chilean_time(date, telescope)
+
+chilean_plans = chilean_time(date, telescope) # Initialize the chilean_time object
 
 df = pd.read_csv('SPIRIT_target_tests.csv')
 
-
-target_chilean = pd.DataFrame({'Date': date, 'Telescope': df['Telescope'][0], 'Name': df['Name'][0],
-                        'Start': df['Start'][0],
-                        'End': df['End'][0],
-                        'RA':  df['RA'][0],
-                        'DEC': df['DEC'][0],
-                        'Filter': 'z',
-                        'texp': 10,
-                        'Counts': counts, '#target': 1, 'Gaia_ID': 'gaia_id', 'Jmag': 19, 'SpT': 'M8'},
+target_chilean = pd.DataFrame({'Date': date, 'Telescope': telescope, 'Name': 'Ch_WASP-157',
+                               'Start': '2021-07-07 22:30:00.000',
+                               'End': '2021-07-08 03:00:00.000',
+                               'RA':   201.6552906,
+                               'DEC': -8.3175976, 'Filter': 'r\'',
+                               'texp': 10,
+                               'Counts': counts, 'Vmag': 12.9, 'SpT': 'G2'},
                               index=[0])
+if chilean_nb_target > 1:
+    other_target = {'Date': date, 'Telescope': telescope, 'Name': 'Ch_HATS-24',
+                    'Start': '2021-07-08 03:00:00.000',
+                    'End': '2021-07-08 09:15:00.000',
+                    'RA':  268.8906975,
+                    'DEC': -61.7473242,
+                    'Filter': 'r\'',
+                    'texp': 10,
+                    'Counts': counts, 'Vmag': 12.83, 'SpT': 'F8'}
+    target_chilean = pd.concat([target_chilean,pd.DataFrame(other_target, index=[0])], ignore_index=True)
 
-
-for i in range(1,2):
-    if chilean_nb_target > 1:
-        other_target = {'Date': date, 'Telescope': df['Telescope'][i], 'Name': df['Name'][i],
-                        'Start': df['Start'][i],
-                        'End': df['End'][i],
-                        'RA':  df['RA'][i],
-                        'DEC': df['DEC'][i],
-                        'Filter': 'z',
-                        'texp': 10,
-                        'Counts': counts, '#target': 1, 'Gaia_ID': 'gaia_id', 'Jmag': 19, 'SpT': 'M8'}
-        target_chilean = pd.concat([target_chilean,pd.DataFrame(other_target,index=[0])], ignore_index=True)
 
 # for i in range(-1, -2):
 #     if chilean_nb_target > 1:
